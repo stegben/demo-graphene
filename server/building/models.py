1d9e519a015bcb07a745e91048c2b3bc37bb042a
cph <stegben.benjamin@gmail.com>
2017-08-16 12:11:44 +0800

    init data

--- server/building/models.py
+++ server/building/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class House(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    house = models.ForeignKey(House, related_name='rooms')
+
+    def __str__(self):
+        return self.name + '_from_' + str(self.house)
